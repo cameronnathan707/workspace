59b55b2be9cd77c8b32aed42020149bbe5e9c0cc
cameronnathan707 <62361875+cameronnathan707@users.noreply.github.com>
2020-03-18 19:08:47 -0600

    added basic discord bot startup

--- bot.py
+++ bot.py
@@ -1,1 +1,11 @@
-print("hello world")
+import discord
+TOKEN = 'Your Secret Token'
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("The bot is ready!")
+    await client.change_presence(activity=discord.Game(name="Music"))
+
+client.run(TOKEN)
